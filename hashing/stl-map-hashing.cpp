fc0227087f74255094bd9e6656e29d0e63a947c9
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-02-11 23:11:50 +0530

    Refactor formatting for consistency in STL map hashing program

--- hashing/stl-map-hashing.cpp
+++ hashing/stl-map-hashing.cpp
@@ -1,24 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
     int arr[n];
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
+    // unordered map can also be used here for better average time complexity O(1) for insert and search operations instead of O(log n) in map but it does not store the values in sorted order like map does.
     map<int, int> mpp; // stores all the values in sorted order
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         mpp[arr[i]]++;
     }
 
     int q;
     cin >> q;
 
-    while (q--) {
+    while (q--)
+    {
         int number;
         cin >> number;
         cout << mpp[number] << endl;
